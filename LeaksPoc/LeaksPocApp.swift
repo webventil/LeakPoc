64c390881b2dc2b56af32e310f0939451c095713
Arne Tempelhof <arnetempelhof@gmail.com>
2021-02-08 07:37:08 +0100

    Initial Commit

---
+++ LeaksPoc/LeaksPocApp.swift
@@ -0,0 +1,17 @@
+//
+//  LeaksPocApp.swift
+//  LeaksPoc
+//
+//  Created by Arne Tempelhof on 08.02.21.
+//
+
+import SwiftUI
+
+@main
+struct LeaksPocApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
